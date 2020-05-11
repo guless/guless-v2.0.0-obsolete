14f65aa92027a92fcfd676cf2cd656eba9f9e5f5
Samlv9 <samlv9@live.com>
2020-05-11 17:12:49 +0800

    默认配置文件。

---
+++ scripts/createDefaultWebpackConfiguration.js
@@ -0,0 +1,20 @@
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/// @Copyright ~2020 ☜Samlv9☞ and other contributors
+/// @MIT-LICENSE | 6.0.1 | https://developers.guless.com/
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function createDefaultWebpackConfiguration(env = {}, argv = {}) {
+    return {
+        resolve: {
+            extensions: [
+                ".tsx",
+                ".ts",
+                ".wasm",
+                ".mjs",
+                ".js",
+                ".json",
+            ],
+        },
+    };
+}
+
+module.exports = createDefaultWebpackConfiguration;
