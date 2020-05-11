0f9ea6f6c12f177d65c94e831349ba98ce6ec364
Samlv9 <samlv9@live.com>
2020-05-11 17:53:58 +0800

    新增断言以及内部属性访问。

---
+++ src/internal.ts
@@ -0,0 +1,6 @@
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/// @Copyright ~2020 ☜Samlv9☞ and other contributors
+/// @MIT-LICENSE | 6.0.1 | https://developers.guless.com/
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+type internal = any;
+export default internal;
