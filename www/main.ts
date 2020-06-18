43f32b945ac9c0dc749a39858c59bb6a69c100f1
Samlv9 <samlv9@live.com>
2020-06-18 18:26:09 +0800

    修复：注释 banner 版本号。

--- www/main.ts
+++ www/main.ts
@@ -1,5 +1,5 @@
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 /// @Copyright ~2020 ☜Samlv9☞ and other contributors
-/// @MIT-LICENSE | 6.0.1 | https://developers.guless.com/
+/// @MIT-LICENSE | 6.0 | https://developers.guless.com/
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 console.log("Hello, World!");
--- www/postcss.config.js
+++ www/postcss.config.js
@@ -1,6 +1,12 @@
-/// For a detailed explanation regarding each configuration property, visit:
-/// https://github.com/postcss/postcss-loader
-/// https://github.com/postcss/postcss/blob/master/docs/plugins.md
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/// @Copyright ~2020 ☜Samlv9☞ and other contributors
+/// @MIT-LICENSE | 6.0 | https://developers.guless.com/
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * @see https://github.com/postcss/postcss-loader
+ * @see https://github.com/postcss/postcss/blob/master/docs/plugins.md
+ */
 function createPostCSSConfiguration(context) {
     return {
         plugins: {
