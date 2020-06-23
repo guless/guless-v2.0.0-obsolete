7983e037d8d79cda566f40a18c9a29e783f925ef
Samlv9 <samlv9@live.com>
2020-06-23 18:03:36 +0800

    新增 PWA 安装功能(manifest.json)支持。

--- www/service-worker.ts
+++ www/service-worker.ts
@@ -2,4 +2,5 @@
 /// @Copyright ~2020 ☜Samlv9☞ and other contributors
 /// @MIT-LICENSE | 6.0.1 | https://developers.guless.com/
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+self.addEventListener("fetch", () => {});
 console.log("Hello from service worker.");
