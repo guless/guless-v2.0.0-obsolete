59baddc599bf0bf765eb6b8b6c3016991a9272c8
Samlv9 <samlv9@live.com>
2020-06-23 18:43:20 +0800

    修复：service-worker.ts 文件声明。

--- www/service-worker.ts
+++ www/service-worker.ts
@@ -2,5 +2,15 @@
 /// @Copyright ~2020 ☜Samlv9☞ and other contributors
 /// @MIT-LICENSE | 6.0.1 | https://developers.guless.com/
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-self.addEventListener("fetch", () => {});
-console.log("Hello from service worker.");
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/// @Copyright ~2020 ☜Samlv9☞ and other contributors
+/// @MIT-LICENSE | 6.0.1 | https://developers.guless.com/
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/// <reference no-default-lib="true"/>
+/// <reference lib="es2015" />
+/// <reference lib="webworker" />
+const serviceWorker: ServiceWorkerGlobalScope = self as any as ServiceWorkerGlobalScope;
+
+serviceWorker.addEventListener("fetch", (evt: FetchEvent) => {
+    /*< empty >*/
+});
