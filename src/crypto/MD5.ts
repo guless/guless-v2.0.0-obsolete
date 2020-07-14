8c7f1e1bd5983d75e58f9e2588cd18bb779807b3
Samlv9 <samlv9@live.com>
2020-07-14 19:37:34 +0800

    定义各种消息摘要算法结构。

---
+++ src/crypto/MD5.ts
@@ -0,0 +1,21 @@
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/// @Copyright ~2020 ☜Samlv9☞ and other contributors
+/// @MIT-LICENSE | 6.0 | https://developers.guless.com/
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import HashAlgorithm from "./HashAlgorithm";
+
+class MD5 extends HashAlgorithm {
+    public reset(): void {
+        throw new Error("Method not implemented.");
+    }
+    
+    public update(input: Uint8Array): void {
+        throw new Error("Method not implemented.");
+    }
+
+    public final(): Uint8Array {
+        throw new Error("Method not implemented.");
+    }
+}
+
+export default MD5;
