678b0dd0fbe3637a1dfaa16bc1523d71f526968e
Samlv9 <samlv9@live.com>
2020-07-15 14:55:25 +0800

    单元测试分组。

--- www/crypto.ts
+++ www/crypto.ts
@@ -31,6 +31,7 @@ function test_md2(value: string, expect: string): void {
     console.log(`%c md2("${value}") => actual:${actual}, expect:${expect};`, `color: ${actual === expect ? "#0a0" : "#a00"};`);
 }
 
+console.group("crc32");
 test_crc32("", 0x00000000);
 test_crc32("a", 0xe8b7be43);
 test_crc32("abc", 0x352441c2);
@@ -38,7 +39,9 @@ test_crc32("message digest", 0x20159d7f);
 test_crc32("abcdefghijklmnopqrstuvwxyz", 0x4c2750bd);
 test_crc32("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", 0x1fc2e6d2);
 test_crc32("12345678901234567890123456789012345678901234567890123456789012345678901234567890", 0x7ca94a72);
+console.groupEnd();
 
+console.group("md2");
 test_md2("", "8350e5a3e24c153df2275c9f80692773");
 test_md2("a", "32ec01ec4a6dac72c0ab96fb34c0b5d1");
 test_md2("abc", "da853b0d3f88d99b30283a69e6ded6bb");
@@ -46,3 +49,4 @@ test_md2("message digest", "ab4f496bfb2a530b219ff33031fe06b0");
 test_md2("abcdefghijklmnopqrstuvwxyz", "4e8ddff3650292ab5a4108c3aa47940b");
 test_md2("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", "da33def2a42df13975352846c30338cd");
 test_md2("12345678901234567890123456789012345678901234567890123456789012345678901234567890", "d5976f79d83d3a0dc9806c3c66f3efd8");
+console.groupEnd();
