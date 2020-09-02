fa1d36df5fa20cf48e608e7c97a53d8088482b9b
Samlv9 <samlv9@live.com>
2020-09-02 18:59:10 +0800

    移除 manifest 资源。

--- www/service-worker.ts
+++ www/service-worker.ts
@@ -2,9 +2,3 @@
 /// @Copyright ~2020 ☜Samlv9☞ and other contributors
 /// @MIT-LICENSE | 6.0 | https://developers.guless.com/
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-/// <reference lib="webworker" />
-const serviceWorker: ServiceWorkerGlobalScope = self as any as ServiceWorkerGlobalScope;
-
-serviceWorker.addEventListener("fetch", (evt: FetchEvent) => {
-    /*< empty >*/
-});
