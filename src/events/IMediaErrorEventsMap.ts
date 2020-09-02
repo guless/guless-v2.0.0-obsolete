1817d8991fb3b0ab0c2d03cf5b50e962bc957721
Samlv9 <samlv9@live.com>
2020-09-02 11:49:02 +0800

    新增 MediaErrorEvent 事件对象。

---
+++ src/events/IMediaErrorEventsMap.ts
@@ -0,0 +1,11 @@
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/// @Copyright ~2020 ☜Samlv9☞ and other contributors
+/// @MIT-LICENSE | 6.0 | https://developers.guless.com/
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import MediaErrorEvent from "./MediaErrorEvent";
+
+interface IMediaErrorEventsMap {
+    [MediaErrorEvent.ERROR]: MediaErrorEvent;
+}
+
+export default IMediaErrorEventsMap;
