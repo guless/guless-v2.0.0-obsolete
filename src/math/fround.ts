3d63f2d1f8c40cdcccbfc2e700688967edf3f80e
Samlv9 <samlv9@live.com>
2020-10-09 14:34:03 +0800

    新增 Math.fround() 函数兼容性补丁。

--- src/math/fround.ts
+++ src/math/fround.ts
@@ -15,7 +15,26 @@ function createMathFRound(): typeof Math.fround {
     }
 
     return function fround(x: number): number {
-        throw new Error(`The "fround()" is not implemented.`);
+        if (x === 0 || isNaN(x)) {
+            return x;
+        }
+    
+        const sign: number = x < 0 ? -1 : 1;
+    
+        if (sign === -1) {
+            x = -x;
+        }
+    
+        const e: number = Math.floor(Math.log(x) / Math.LN2);
+        const p: number = Math.pow(2, Math.max(-126, Math.min(e, 127)));
+        const l: number = e < -127 ? 0 : 1;
+        const m: number = Math.round((l - x / p) * 0x800000);
+    
+        if (m <= -0x800000) {
+            return sign * Infinity;
+        }
+    
+        return sign * p * (l - m / 0x800000);
     }
 }
 
