4f954b037ecce056d5bdb3680d9cceaa36e9f4fc
Samlv9 <samlv9@live.com>
2020-10-12 15:17:25 +0800

    新增 TextEncoder/TextDecoder 单元测试。

---
+++ test/TextEncoder.test.ts
@@ -0,0 +1,15 @@
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/// @Copyright ~2020 ☜Samlv9☞ and other contributors
+/// @MIT-LICENSE | 6.0 | https://developers.guless.com/
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import TextEncoder from "@/text/TextEncoder";
+import TextDecoder from "@/text/TextDecoder";
+import allocUint8Array from "@/buffer/allocUint8Array";
+
+test("encode & decode", () => {
+    const source: string = "123abc$£¥中国😄";
+    const expected: Uint8Array = allocUint8Array([49, 50, 51, 97, 98, 99, 36, 194, 163, 194, 165, 228, 184, 173, 229, 155, 189, 240, 159, 152, 132]);
+
+    expect(new TextEncoder().encode(source)).toEqual(expected);
+    expect(new TextDecoder().decode(expected)).toBe(source);
+});
