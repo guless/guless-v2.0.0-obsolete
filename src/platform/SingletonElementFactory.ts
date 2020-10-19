3e4cc8b73f88a2c8df63ac707824fcd29d5708bc
Samlv9 <samlv9@live.com>
2020-10-19 15:58:43 +0800

    新增：HTMLElement 单例工厂。

---
+++ src/platform/SingletonElementFactory.ts
@@ -0,0 +1,33 @@
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/// @Copyright ~2020 ☜Samlv9☞ and other contributors
+/// @MIT-LICENSE | 6.0 | https://developers.guless.com/
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class SingletonElementFactory {
+    private static readonly __REGISTRY__: Record<string, HTMLElement> = Object.create(null);
+
+    public static create<K extends keyof HTMLElementTagNameMap>(tagName: K): HTMLElementTagNameMap[K];
+    public static create<K extends keyof HTMLElementDeprecatedTagNameMap>(tagName: K): HTMLElementDeprecatedTagNameMap[K];
+    public static create(tagName: string): HTMLElement;
+    public static create(tagName: string): HTMLElement {
+        if (!SingletonElementFactory.contains(tagName)) {
+            SingletonElementFactory.register(tagName, document.createElement(tagName));
+        }
+        return SingletonElementFactory.__REGISTRY__[tagName];
+    }
+
+    public static contains<K extends keyof HTMLElementTagNameMap>(tagName: K): boolean;
+    public static contains<K extends keyof HTMLElementDeprecatedTagNameMap>(tagName: K): boolean;
+    public static contains(tagName: string): boolean;
+    public static contains(tagName: string): boolean {
+        return !!SingletonElementFactory.__REGISTRY__[tagName];
+    }
+
+    public static register<K extends keyof HTMLElementTagNameMap>(tagName: K, element: HTMLElement): void;
+    public static register<K extends keyof HTMLElementDeprecatedTagNameMap>(tagName: K, element: HTMLElement): void;
+    public static register(tagName: string, element: HTMLElement): void;
+    public static register(tagName: string, element: HTMLElement): void {
+        SingletonElementFactory.__REGISTRY__[tagName] = element;
+    }
+}
+
+export default SingletonElementFactory;
--- src/platform/capabilities/common-shared-elements.ts
+++
@@ -1,5 +0,0 @@
-/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-/// @Copyright ~2020 ☜Samlv9☞ and other contributors
-/// @MIT-LICENSE | 6.0 | https://developers.guless.com/
-/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-export const HTML_MEDIA_ELEMENT: HTMLMediaElement = document.createElement("video");
