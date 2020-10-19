8789deff2891b12e1cea1a5c975e5c7a82122ede
Samlv9 <samlv9@live.com>
2020-10-19 16:12:37 +0800

    重构变量名称。

--- src/platform/SingletonElementFactory.ts
+++ src/platform/SingletonElementFactory.ts
@@ -3,7 +3,7 @@
 /// @MIT-LICENSE | 6.0 | https://developers.guless.com/
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class SingletonElementFactory {
-    private static readonly __REGISTRY__: Record<string, HTMLElement> = Object.create(null);
+    private static readonly __ELEMENT_MAP__: Record<string, HTMLElement> = Object.create(null);
 
     public static create<K extends keyof HTMLElementTagNameMap>(tagName: K): HTMLElementTagNameMap[K];
     public static create<K extends keyof HTMLElementDeprecatedTagNameMap>(tagName: K): HTMLElementDeprecatedTagNameMap[K];
@@ -12,21 +12,21 @@ class SingletonElementFactory {
         if (!SingletonElementFactory.contains(tagName)) {
             SingletonElementFactory.register(tagName, document.createElement(tagName));
         }
-        return SingletonElementFactory.__REGISTRY__[tagName];
+        return SingletonElementFactory.__ELEMENT_MAP__[tagName];
     }
 
     public static contains<K extends keyof HTMLElementTagNameMap>(tagName: K): boolean;
     public static contains<K extends keyof HTMLElementDeprecatedTagNameMap>(tagName: K): boolean;
     public static contains(tagName: string): boolean;
     public static contains(tagName: string): boolean {
-        return !!SingletonElementFactory.__REGISTRY__[tagName];
+        return !!SingletonElementFactory.__ELEMENT_MAP__[tagName];
     }
 
     public static register<K extends keyof HTMLElementTagNameMap>(tagName: K, element: HTMLElement): void;
     public static register<K extends keyof HTMLElementDeprecatedTagNameMap>(tagName: K, element: HTMLElement): void;
     public static register(tagName: string, element: HTMLElement): void;
     public static register(tagName: string, element: HTMLElement): void {
-        SingletonElementFactory.__REGISTRY__[tagName] = element;
+        SingletonElementFactory.__ELEMENT_MAP__[tagName] = element;
     }
 }
 
