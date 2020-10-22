d94266c1b4156b3f75cc0bdbd306a55cfde3bd81
Samlv9 <samlv9@live.com>
2020-10-22 10:27:52 +0800

    重构类型名称。

--- src/platform/SharedElementRegistry.ts
+++ src/platform/SharedElementRegistry.ts
@@ -2,32 +2,32 @@
 /// @Copyright ~2020 ☜Samlv9☞ and other contributors
 /// @MIT-LICENSE | 6.0 | https://developers.guless.com/
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-class SingletonElementRegistry {
+class SharedElementRegistry {
     private static readonly __ELEMENT_MAP__: Record<string, HTMLElement> = Object.create(null);
 
     public static create<K extends keyof HTMLElementTagNameMap>(tagName: K): HTMLElementTagNameMap[K];
     public static create<K extends keyof HTMLElementDeprecatedTagNameMap>(tagName: K): HTMLElementDeprecatedTagNameMap[K];
     public static create(tagName: string): HTMLElement;
     public static create(tagName: string): HTMLElement {
-        if (!SingletonElementRegistry.contains(tagName)) {
-            SingletonElementRegistry.register(tagName, document.createElement(tagName));
+        if (!SharedElementRegistry.contains(tagName)) {
+            SharedElementRegistry.register(tagName, document.createElement(tagName));
         }
-        return SingletonElementRegistry.__ELEMENT_MAP__[tagName];
+        return SharedElementRegistry.__ELEMENT_MAP__[tagName];
     }
 
     public static contains<K extends keyof HTMLElementTagNameMap>(tagName: K): boolean;
     public static contains<K extends keyof HTMLElementDeprecatedTagNameMap>(tagName: K): boolean;
     public static contains(tagName: string): boolean;
     public static contains(tagName: string): boolean {
-        return !!SingletonElementRegistry.__ELEMENT_MAP__[tagName];
+        return !!SharedElementRegistry.__ELEMENT_MAP__[tagName];
     }
 
     public static register<K extends keyof HTMLElementTagNameMap>(tagName: K, element: HTMLElement): void;
     public static register<K extends keyof HTMLElementDeprecatedTagNameMap>(tagName: K, element: HTMLElement): void;
     public static register(tagName: string, element: HTMLElement): void;
     public static register(tagName: string, element: HTMLElement): void {
-        SingletonElementRegistry.__ELEMENT_MAP__[tagName] = element;
+        SharedElementRegistry.__ELEMENT_MAP__[tagName] = element;
     }
 }
 
-export default SingletonElementRegistry;
+export default SharedElementRegistry;
--- src/platform/capabilities/supported-media-src-object-attribute.ts
+++ src/platform/capabilities/supported-media-src-object-attribute.ts
@@ -2,5 +2,5 @@
 /// @Copyright ~2020 ☜Samlv9☞ and other contributors
 /// @MIT-LICENSE | 6.0 | https://developers.guless.com/
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-import SingletonElementRegistry from "../SingletonElementRegistry";
-export const SUPPORTED_MEDIA_SRC_OBJECT_ATTRIBUTE: boolean = (typeof SingletonElementRegistry.create("video").srcObject !== "undefined");
+import SharedElementRegistry from "../SharedElementRegistry";
+export const SUPPORTED_MEDIA_SRC_OBJECT_ATTRIBUTE: boolean = (typeof SharedElementRegistry.create("video").srcObject !== "undefined");
