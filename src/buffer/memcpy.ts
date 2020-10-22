ef2fee58b7b729973aa895b032a7b6029a354ad0
Samlv9 <samlv9@live.com>
2020-10-22 19:49:26 +0800

    优化 memcpy 实现。

--- src/buffer/memcpy.ts
+++ src/buffer/memcpy.ts
@@ -3,16 +3,12 @@
 /// @MIT-LICENSE | 6.0 | https://developers.guless.com/
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 import internal from "../internal";
+import memslc from "./memslc";
 
 function memcpy<T extends Int8Array | Int16Array | Int32Array | Uint8Array | Uint16Array | Uint32Array | Float32Array | Float64Array>(source: T, target: T, sourceStart: number = 0, sourceEnd: number = source.length, targetStart: number = 0, targetEnd: number = target.length): void {
-    if (typeof (target as internal).set === "function" && typeof (source as internal).subarray === "function") {
+    if (typeof (target as internal).set === "function") {
         sourceEnd = sourceStart + Math.min(sourceEnd - sourceStart, targetEnd - targetStart);
-
-        if (sourceStart === 0 && sourceEnd === source.length) {
-            (target as internal).set(source, targetStart);
-        } else {
-            (target as internal).set((source as internal).subarray(sourceStart, sourceEnd), targetStart);
-        }
+        (target as internal).set(memslc(source, sourceStart, sourceEnd), targetStart);
     } else {
         for (let i: number = sourceStart, j: number = targetStart; i < sourceEnd && j < targetEnd; ++i, ++j) {
             target[j] = source[i];
