ee3cc5b3df8c3e87b582a72ae0f4a2f007f8807c
Samlv9 <samlv9@live.com>
2020-10-22 10:23:53 +0800

    重构类型名称。

--- src/platform/capabilities/supported-media-src-object-attribute.ts
+++ src/platform/capabilities/supported-media-src-object-attribute.ts
@@ -2,5 +2,5 @@
 /// @Copyright ~2020 ☜Samlv9☞ and other contributors
 /// @MIT-LICENSE | 6.0 | https://developers.guless.com/
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-import SingletonElementFactory from "../SingletonElementFactory";
-export const SUPPORTED_MEDIA_SRC_OBJECT_ATTRIBUTE: boolean = (typeof SingletonElementFactory.create("video").srcObject !== "undefined");
+import SingletonElementRegistry from "../SingletonElementRegistry";
+export const SUPPORTED_MEDIA_SRC_OBJECT_ATTRIBUTE: boolean = (typeof SingletonElementRegistry.create("video").srcObject !== "undefined");
