bb528983541a96f058391c821d26fa07ef9bd4f6
Samlv9 <samlv9@live.com>
2020-10-28 17:36:54 +0800

    优化 memshr 和 memcpy。

--- src/buffer/memcpy.ts
+++ src/buffer/memcpy.ts
@@ -3,13 +3,15 @@
 /// @MIT-LICENSE | 6.0 | https://developers.guless.com/
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 import internal from "../internal";
-import memslc from "./memslc";
 
 function memcpy<T extends Int8Array | Int16Array | Int32Array | Uint8Array | Uint16Array | Uint32Array | Float32Array | Float64Array>(source: T, target: T, sourceStart: number = 0, sourceEnd: number = source.length, targetStart: number = 0, targetEnd: number = target.length): void {
     if (typeof (target as internal).set === "function") {
         sourceEnd = sourceStart + Math.min(sourceEnd - sourceStart, targetEnd - targetStart);
-        if (sourceStart < sourceEnd) {
-            (target as internal).set(memslc(source, sourceStart, sourceEnd), targetStart);
+
+        if (sourceStart === 0 && sourceEnd === source.length) {
+            (target as internal).set(source, targetStart);
+        } else {
+            (target as internal).set((source as internal).subarray(sourceStart, sourceEnd), targetStart);
         }
     } else {
         for (let i: number = sourceStart, j: number = targetStart; i < sourceEnd && j < targetEnd; ++i, ++j) {
--- src/buffer/memshr.ts
+++ src/buffer/memshr.ts
@@ -4,14 +4,15 @@
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 import internal from "../internal";
 
-function memslc<T extends Int8Array | Int16Array | Int32Array | Uint8Array | Uint16Array | Uint32Array | Float32Array | Float64Array>(source: T, start: number = 0, end: number = source.length): typeof source {
+function memshr<T extends Int8Array | Int16Array | Int32Array | Uint8Array | Uint16Array | Uint32Array | Float32Array | Float64Array>(source: T, start: number = 0, end: number = source.length): typeof source {
     if (start === 0 && end === source.length) {
         return source;
     }
     if (typeof (source as internal).subarray === "function") {
         return (source as internal).subarray(start, end);
     }
-    return (source as internal).slice(start, end);
+    (source as internal).length = end - start;
+    return source;
 }
 
-export default memslc;
+export default memshr;
